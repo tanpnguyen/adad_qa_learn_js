2cc92be80e0af1bf106e7d314960500fd9bc11b6
maiphan1002 <maiphan@kms-technology.com>
2020-02-26 20:05:26 +0700

    Practice 2

--- 1-basics/practice2.js
+++ 1-basics/practice2.js
@@ -12,3 +12,76 @@ John and Mike both play basketball in different teams. In the latest 3 games, Jo
 4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the average winner to the console. HINT: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem :)
 5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
 */
+
+let scoreOfJohn = [89, 120, 103];
+let scoreOfMike = [116, 94, 123];
+
+function calculate(array) {
+  	var sum = 0;
+  	var count = array.length;
+	if (array.length == 0) 
+		return 0;
+  	for (var i = 0; i < count; i++) {
+    		sum = sum + array[i];
+  	}
+  	return sum / count;
+}
+
+var averageScoreOfJohn = calculate(scoreOfJohn);
+var averageScoreOfMike = calculate(scoreOfMike);
+
+console.log("Question 1");
+console.log("Average score of John's team: " + averageScoreOfJohn);
+console.log("Average score of Mike's team: " + averageScoreOfMike);
+
+console.log("Question 2, 3");
+if (averageScoreOfJohn > averageScoreOfMike) {
+	console.log("The winner team is John's team. Average score of John's team: " + averageScoreOfJohn);
+}
+else if (averageScoreOfJohn < averageScoreOfMike) {
+	console.log("The winner team is Mike's team. Average score of Mike's team: " + averageScoreOfMike);
+}
+else {
+	console.log("2 teams has equal average score.");
+}
+
+let scoreOfMary = [97, 134, 105];
+var averageScoreOfMary = calculate(scoreOfMary);
+
+console.log("Question 4, 5");
+console.log("Average score of Mary's team: " + averageScoreOfMary);
+
+if (averageScoreOfJohn > averageScoreOfMike) {
+	if (averageScoreOfJohn > averageScoreOfMary) {
+		console.log("The winner team is John's team. Average score of John's team: " + averageScoreOfJohn);
+	}
+	else if (averageScoreOfJohn < averageScoreOfMary) {
+		console.log("The winner team is Mary's team. Average score of Mary's team: " + averageScoreOfMary);
+	}
+	else if (averageScoreOfJohn == averageScoreOfMary) {
+		console.log("The winner teams are John's team & Mary's team. Average score of 2 teams: " + averageScoreOfJohn);
+	}
+}
+else if (averageScoreOfJohn < averageScoreOfMike) {
+	if (averageScoreOfMike > averageScoreOfMary) {
+		console.log("The winner team is Mike's team. Average score of Mike's team: " + averageScoreOfMike);
+	}
+	else if (averageScoreOfMike < averageScoreOfMary) {
+		console.log("The winner team is Mary's team. Average score of Mary's team: " + averageScoreOfMary);
+	}
+	else if (averageScoreOfMike == averageScoreOfMary) {
+		console.log("The winner teams are Mike's team & Mary's team. Average score of 2 teams: " + averageScoreOfMike);
+	}
+
+}
+else if (averageScoreOfJohn == averageScoreOfMike) {
+	if (averageScoreOfJohn > averageScoreOfMary) {
+		console.log("The winner teams are John's team & Mike's team. Average score of 2 teams: " + averageScoreOfJohn);
+	}
+	else if (averageScoreOfJohn < averageScoreOfMary) {
+		console.log("The winner team is Mary's team. Average score of Mary's team: " + averageScoreOfMary);
+	}
+	else if (averageScoreOfJohn == averageScoreOfMary) {
+		console.log("The all teams has equal score. Average score of all teams: " + averageScoreOfJohn);
+	}
+}
