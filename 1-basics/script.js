d38ea42cb5c9c575479433c2e2b28761752c8fe1
danhthanhnguyen <danhthanhnguyen@kms-technology.com>
2020-02-26 14:40:52 +0700

    Commit practice 1 - Danh Nguyen

--- 1-basics/script.js
+++ 1-basics/script.js
@@ -10,3 +10,11 @@ Mark and John are trying to compare their BMI (Body Mass Index), which is calcul
 3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
 4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true"). 
 */
+
+
+function getBMI (mass, height){
+    return mass/(height*2)
+}
+var massMark = 60, massJohn = 70, heightMark = 170, heightJohn = 175;
+var bmiMarkHigherThanJohn = getBMI (massMark, heightMark) > getBMI (massJohn, heightJohn);
+console.log("Is Mark's BMI higher than John's? " + bmiMarkHigherThanJohn);
