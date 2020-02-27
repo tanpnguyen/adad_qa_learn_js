3f39b8f8ef3166dd1d29f9ee631600e5d4191af5
tanpnguyen <ta.nguyen@ext.tricentis.com>
2020-02-27 10:36:01 +0700

    Tan Nguyen's practice 2

--- 1-basics/practice2.js
+++ 1-basics/practice2.js
@@ -12,3 +12,31 @@ John and Mike both play basketball in different teams. In the latest 3 games, Jo
 4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the average winner to the console. HINT: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem :)
 5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
 */
+
+let miKeTeamScore = [89, 120, 103]
+let johnTeamScore = [116, 94, 123]
+
+let arrAvg = arr => arr.reduce((a, b) => a + b, 0) / arr.length
+
+let teamWinMessage = (arrAvg(miKeTeamScore) > arrAvg(johnTeamScore)) ? 'Mike\'s team win' : (arrAvg(miKeTeamScore) < arrAvg(johnTeamScore)) ? 'John\'s team win' : 'Two teams are equal'
+
+
+console.log(teamWinMessage)
+
+
+let maryTeamScore = [116, 94, 123]
+
+let maxAvg = arr => Math.max(...arr);
+
+let maxResult = maxAvg([arrAvg(miKeTeamScore), arrAvg(johnTeamScore), arrAvg(maryTeamScore)])
+
+
+let teamMaxAvgMessage = (maxResult === arrAvg(miKeTeamScore) && maxResult === arrAvg(johnTeamScore) && maxResult != arrAvg(maryTeamScore)) ? 'Mike\'s and John\'s team win'
+    : (maxResult === arrAvg(miKeTeamScore) && maxResult === arrAvg(maryTeamScore) && maxResult != arrAvg(johnTeamScore)) ? 'Mike\'s and Marry\'s team win'
+        : (maxResult === arrAvg(maryTeamScore) && maxResult === arrAvg(johnTeamScore) && maxResult != arrAvg(miKeTeamScore)) ? 'Marry\'s and John\'s team win'
+            : (maxResult == arrAvg(miKeTeamScore) && maxResult != arrAvg(johnTeamScore) && maxResult != arrAvg(maryTeamScore)) ? 'Mike\'s team win'
+                : (maxResult === arrAvg(johnTeamScore) && maxResult != arrAvg(miKeTeamScore) && maxResult != arrAvg(maryTeamScore)) ? 'John\'s team win'
+                    : (maxResult === arrAvg(maryTeamScore) && maxResult != arrAvg(miKeTeamScore) && maxResult != arrAvg(johnTeamScore)) ? 'Marry\'s team win'
+                        : 'Three teams are equal'
+
+console.log(teamMaxAvgMessage)
