d88318247a3a3f3a73e20fe8b92e717e422c86d3
maiphan1002 <maiphan@kms-technology.com>
2020-02-28 09:09:40 +0700

    Practice 3

--- 1-basics/practice3.js
+++ 1-basics/practice3.js
@@ -13,3 +13,44 @@ In the end, John would like to have 2 arrays:
 
 
 */
+
+var firstBill = 124;
+var secondBill = 48;
+var thirdBill = 268;
+
+function calculateTip (bill) {
+	if (bill < 50 && bill > 0) {
+		tip = 0.2 * bill;
+	}
+	else if (bill >= 50 && bill <= 200) {
+		tip = 0.15 * bill;
+	}
+	else if (bill > 200) {
+		tip = 0.1 * bill;
+	}
+	else if (bill <= 0) {
+		return console.log("Bill required greater than 0$");
+	}
+	return tip;
+}
+
+function calculateFinalPaidAmounts (bill, tip) {
+	if (bill <= 0 || tip <= 0) {
+		return console.log ("Bill or tip required greater than 0$");
+	}
+	return bill + tip;
+}
+
+var tipOfFirstBill = calculateTip (firstBill);
+var tipOfSecondBill = calculateTip (secondBill);
+var tipOfThirdBill = calculateTip (thirdBill);
+
+let arrayOfTip = [tipOfFirstBill, tipOfSecondBill, tipOfThirdBill];
+console.log ("All three tips: " + arrayOfTip);
+
+var totalOfFirstBill = calculateFinalPaidAmounts (firstBill, tipOfFirstBill);
+var totalOfSecondBill = calculateFinalPaidAmounts (secondBill, tipOfSecondBill);
+var totalOfThirdBill = calculateFinalPaidAmounts (thirdBill, tipOfThirdBill);
+
+let arrayOfFinalPaidAmounts = [totalOfFirstBill, totalOfSecondBill, totalOfThirdBill];
+console.log ("All three final paid amounts: " + arrayOfFinalPaidAmounts);
