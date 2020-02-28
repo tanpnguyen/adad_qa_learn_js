6f680a114e8ca564e227203c07a00d1b9ecabe1a
maiphan1002 <maiphan@kms-technology.com>
2020-02-28 16:01:14 +0700

    Practice 4

--- 1-basics/practice4.js
+++ 1-basics/practice4.js
@@ -11,3 +11,36 @@ Let's remember the first coding challenge where Mark and John compared their BMI
 Remember: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
 
 */
+
+function getBMI (mass, height){
+	if (height == 0)
+		return 0;
+	return mass/(height*2);
+}
+
+let person1 = {
+	"name": "Mark",
+	"mass": 60,
+	"height": 1.7
+}
+
+
+let person2 = {
+	"name": "John",
+	"mass": 60,
+	"height": 1.7
+}
+
+person1.BMI = getBMI (person1.mass, person1.height);
+person2.BMI = getBMI (person2.mass, person2.height);
+
+
+
+let message = (person1.BMI > person2.BMI) ? "The highest BMI is " + person1.name + ". The respective BMI is " + person1.BMI : 
+        (person1.BMI < person2.BMI) ? "The highest BMI is " + person2.name + ". The respective BMI is " + person2.BMI:
+        "2 people have the same BMI" ;
+
+console.log("The BMI of Mark is " + person1.BMI);
+console.log("The BMI of John is " + person2.BMI);        
+console.log(message);
+
