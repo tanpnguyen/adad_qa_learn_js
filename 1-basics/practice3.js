fe2759227f6060329a220a4045177c271aaff9a5
a.nguyenthivan <anhtvnguyen@kms-technology.com>
2020-02-28 09:39:03 +0700

    AnhNguyen commit practice 3

--- 1-basics/practice3.js
+++ 1-basics/practice3.js
@@ -13,3 +13,48 @@ In the end, John would like to have 2 arrays:
 
 
 */
+var bill;
+var billTipList = [];
+var billPaidWithTip = [];
+var billList = [124,48,268];
+var i;
+
+//This's a function to calculate tip
+function tipCalculation (bill){
+	var tip;
+	if (bill > 0 && bill < 50){
+		tip = bill * 0.2;
+		
+	} else if (bill >= 50 && bill <= 200){
+		tip = bill * 0.15;
+		
+	} else if (bill > 200){
+		tip = bill * 0.1;
+		
+	} else {
+		console.log("Having Invalid bill");
+		tip = 0;
+	}
+	return tip;
+}
+
+//Genereate an array of 3 tips based on 3 bills. Bills and Bills paid are formatted with two decimals.
+  
+for (i = 0; i <= billList.length - 1; i++){
+	//If Bill is invalid, it will be set to 0.
+	if (isNaN(billList[i]) || (billList[i] === " ")) {
+		billList[i] = 0;
+	};
+	
+	billTipList[i] = tipCalculation(billList[i]);
+	billTipList[i] = parseFloat(billTipList[i]).toFixed(2);
+		console.log("\n Bill " + (i+1) + ": $"  + parseFloat(billList[i]*1).toFixed(2) + " has tip $" + billTipList[i]);
+	billList[i] = parseFloat(billList[i]).toFixed(2);
+	billPaidWithTip[i] = (+billList[i]) + (+billTipList[i]);
+	billPaidWithTip[i] = parseFloat(billPaidWithTip[i]).toFixed(2);
+}
+console.log("\n List of Tips of all bills: " + billTipList.toString());
+console.log('\n List of Bills paid with Tips: ' + billPaidWithTip.toString());
+
+
+
