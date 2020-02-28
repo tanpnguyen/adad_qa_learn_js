de3e46487e505f699b4d0f501df10a84015a1766
maiphan1002 <maiphan@kms-technology.com>
2020-02-28 19:15:07 +0700

    Update Practice 4

--- 1-basics/practice4.js
+++ 1-basics/practice4.js
@@ -11,6 +11,44 @@ Let's remember the first coding challenge where Mark and John compared their BMI
 Remember: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
 
 */
+let person1 = {
+	name: "Mark",
+	mass: 60,
+	height: 1.7,
+	getBMI (mass, height) {
+		if (height == 0)
+			return 0;
+		return mass/(height*2);
+	}
+}
+
+person1.BMI = person1.getBMI(person1.mass, person1.height)
+console.log("Info of Mark: " + JSON.stringify(person1, null, 4));
+
+
+let person2 = {
+	name: "John",
+	mass: 60,
+	height: 1.7,
+	getBMI (mass, height) {
+		if (height == 0)
+			return 0;
+		return mass/(height*2);
+	}
+}
+
+person2.BMI = person2.getBMI(person2.mass, person2.height)
+console.log("Info of John " + JSON.stringify(person2, null, 4));
+
+let message = (person1.BMI > person2.BMI) ? "The highest BMI is " + person1.name + ". The respective BMI is " + person1.BMI : 
+        (person1.BMI < person2.BMI) ? "The highest BMI is " + person2.name + ". The respective BMI is " + person2.BMI:
+        "2 people have the same BMI" ;
+
+console.log("The BMI of Mark is " + person1.BMI);
+console.log("The BMI of John is " + person2.BMI);        
+console.log(message);
+
+------------------------------------------------
 
 function getBMI (mass, height){
 	if (height == 0)
