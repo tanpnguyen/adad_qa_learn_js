b1a905d63ad1dc1a983efab5af6e32b7b0e824ef
danhthanhnguyen <danhthanhnguyen@kms-technology.com>
2020-03-06 16:32:30 +0700

    Practice 6 - Danh Nguyen

--- 1-basics/practice6.js
+++ 1-basics/practice6.js
@@ -23,3 +23,47 @@ c) correct answer (I would use a number for this)
 
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
+//  1 
+(function() {
+let Question = function (question, answers, correctAnswer){
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+}
+
+//  2
+var question1 = new Question("Which is the largest country in the world?", ["0. China","1. Russia","2. Brazil","3. America"], 1);
+var question2 = new Question("Which is the biggest animal in the world?", ["0. Elephant","1. Hippo","2. Blue Whale","3. Shark"], 2);
+
+// 3
+var questionArray = [question1, question2];
+
+//4
+Question.prototype.printQuestion = function(){
+    console.log(this.question)
+    for(let i in this.answers){
+        console.log(this.answers[i])
+    }
+}
+let numberOfQuestion = Math.floor(Math.random() * 2);
+questionArray[numberOfQuestion].printQuestion();
+
+//5 - 6
+Question.prototype.checkCorrectAnswer = function(answerOfUser){
+    if(answerOfUser == this.correctAnswer){
+        console.log ("Correct answer !!!");
+    }
+    else{
+        console.log ("Incorrect answer !!!")
+    }
+}
+var answerOfUser = prompt("Please enter your answer");
+questionArray[numberOfQuestion].checkCorrectAnswer(answerOfUser);
+
+})();
+
+
+
+
+
+
