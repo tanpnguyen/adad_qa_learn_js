f1d0f7391e0ca77d66da77b5d1b35e376faf8547
maiphan1002 <maiphan@kms-technology.com>
2020-03-06 14:09:42 +0700

    Practice 6

--- 1-basics/practice6.js
+++ 1-basics/practice6.js
@@ -23,3 +23,35 @@ c) correct answer (I would use a number for this)
 
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
+
+--------------
+(function() {
+var Question = function (question, answers, correctAnswer) {
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+}
+
+var question1 = new Question ('How many days in week', ['5', '6', '7'], 2);
+var question2 = new Question ('How many weeks in year', ['52', '53', '54'], 0);
+var listQuestions = [question1, question2];
+
+Question.prototype.displayQuestion = function() {
+	console.log(this.question);
+	for (let i = 0; i < this.answers.length; i++) {
+        	console.log(i + '. ' + this.answers[i]);
+	}
+}
+
+Question.prototype.checkCorrectAnswer = function(answer) {
+	let message = (answer == this.correctAnswer) ? "Correct answer" : "Incorrect answer";
+	console.log(message);
+}	      
+
+var random = Math.floor(Math.random() * listQuestions.length);
+listQuestions[random].displayQuestion();
+listQuestions[random].checkCorrectAnswer(parseInt(prompt('Choose answer')));
+
+})();
+
+
